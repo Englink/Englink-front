fd4917db4aad7d29810a13e5a5da0c10f9eb89b6
nadavde <nadav2546@gmail.com>
2024-05-09 11:50:58 +0300

    add page

--- teacher-plus/src/App.jsx
+++ teacher-plus/src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-
-    <h1 className="text-3xl font-bold underline">
-      haimm
-    </h1>
-
-    </>
-  )
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/loginPage" element={<LoginPage />} />
+          <Route path="/signUpPage" element={<SignUpPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+
+
+export default App;
+
+
+
---
+++ teacher-plus/src/pages/LoginPage.jsx
@@ -0,0 +1,173 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import './loginPage.css'
+import MainPage from './mainPage'
+
+function LoginPage() {
+  const [userName, setUserName] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post('http://localhost:3003/api/students/login', {
+        userDetails: {
+          email: userName,
+          password: userPassword
+        },
+        isStudent: true
+      });
+
+      alert('נכנסת למערכת בהצלחה!')
+      console.log(response.data.token)
+
+      setUserName('')
+      setUserPassword('')
+      
+
+      setLoggedIn(true)
+    } catch (error) {
+      alert('שגיאה במהלך התחברות: ' + error.toString());
+    }
+  };
+
+  // ניתן להציג את הקומפוננטה של MainPage אם המשתמש מחובר
+  if (loggedIn) {
+    return <MainPage />;
+  }
+
+  // נוסיף את הפונקציונאליות של התחברות רק אם המשתמש לא מחובר
+  return (
+    <form onSubmit={handleLogin}>
+      <label htmlFor="userName">שם משתמש:</label>
+      <input type="text" id="userName" autoComplete='false' value={userName} onChange={(e) => setUserName(e.target.value)} />
+
+      <label htmlFor="userPassword">סיסמה:</label>
+      <input type="password" id="userPassword" autoComplete='false' value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
+
+      <button type="submit">התחבר</button>
+    </form>
+  );
+}
+
+export default LoginPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { BrowserRouter,  useHistory } from 'react-router-dom'; // ייבוא פונקציית הניווט
+// import axios from 'axios';
+// import './loginPage.css';
+
+
+// function LoginPage() {
+//   const [userName, setUserName] = useState('');
+//   const [userPassword, setUserPassword] = useState('');
+//   const history = useHistory(); // השמת פונקציית הניווט במשתנה
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await axios.post('http://localhost:3003/api/students/login', {
+//         userDetails: {
+//           email: userName,
+//           password: userPassword
+//         },
+//         isStudent: true
+//       });
+
+//       alert('נכנסת למערכת בהצלחה!');
+//       console.log(response.data.token);
+      
+//       setUserName('');
+//       setUserPassword('');
+      
+//       // אם התחבר בהצלחה, מעביר אותו לדף הראשי (mainPage)
+//       history.push('/');
+//     } catch (error) {
+//       alert('שגיאה במהלך התחברות: ' + error.toString());
+//     }
+//   };
+
+//   return (
+//     <form onSubmit={handleLogin}>
+//       <label htmlFor="userName">שם משתמש:</label>
+//       <input type="text" id="userName" autoComplete='false' value={userName} onChange={(e) => setUserName(e.target.value)} />
+
+//       <label htmlFor="userPassword">סיסמה:</label>
+//       <input type="password" id="userPassword" autoComplete='false' value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
+
+//       <button type="submit">התחבר</button>
+//     </form>
+//   );
+// }
+
+// export default LoginPage;
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import './loginPage.css';
+
+
+// function LoginPage() {
+//   const [userName, setUserName] = useState('');
+//   const [userPassword, setUserPassword] = useState('');
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await axios.post('http://localhost:3003/api/students/login', {
+//         userDetails: {
+//           email: userName,
+//           password: userPassword
+//         },
+//         isStudent: true
+//       });
+
+//       alert('נכנסת למערכת בהצלחה!');
+//       console.log(response.data.token);
+      
+//       setUserName('');
+//       setUserPassword('');
+//     } catch (error) {
+//       alert('שגיאה במהלך התחברות: ' + error.toString());
+//     }
+//   };
+
+//   return (
+//     <form onSubmit={handleLogin}>
+//       <label htmlFor="userName">שם משתמש:</label>
+//       <input type="text" id="userName" autoComplete='false' value={userName} onChange={(e) => setUserName(e.target.value)} />
+
+//       <label htmlFor="userPassword">סיסמה:</label>
+//       <input type="password" id="userPassword" autoComplete='false' value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
+
+//       <button type="submit">התחבר</button>
+//     </form>
+//   );
+// }
+
+// export default LoginPage;
