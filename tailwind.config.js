b37ebbc63122deba7ac8a1688b8e5e2a4b02922e
shlomohalevi <shlomoww@gmail.com>
2024-06-06 11:01:50 +0300

    login student update

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#0d47a1',      // כחול כהה
-        medium: '#1976d2',       // כחול בינוני
-        light: '#bbdefb',        // תכלת
-        sea: '#0288d1',          // כחול ים לכפתורים
-        veryLight: '#e3f2fd',    // כחול בהיר מאוד לרקע
+        // primary: '#0d47a1',      // כחול כהה
+        // medium: '#1976d2',       // כחול בינוני
+        // light: '#bbdefb',        // תכלת
+        // sea: '#0288d1',          // כחול ים לכפתורים
+        // veryLight: '#e3f2fd',    // כחול בהיר מאוד לרקע
+        blue: '#4b42f7', //button
+        darkblue: '#13131c',//headline
+        gray: '#505050',//paragraf
+        light: '#eae9ff',//spetial button
       },
     },
   },
